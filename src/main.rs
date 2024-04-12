96116941e14957a237c9e894d78e6c52672c7842
Zero <zero@zero-ms7c56.ignes-zero.com>
2024-04-11 22:50:36 -0400

    Add clap
    This is stil raw code.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
+use clap::{Parser};
 use rand::{Rng, thread_rng};
-use std::char;
 use rand::seq::SliceRandom;
-use std::str;
+use std::{char, str};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    digit: bool,
+    #[arg(short, long, value_parser=(clap::value_parser!(u8)))]
+    len: Option<u8>,
+}
 
 fn main() {
+    let args = Cli::parse();
+    println!("digit: {}", args.digit);
+    println!("len: {}", args.len.unwrap_or(10));
+
     const CAPITAL: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const LOWER: &[u8] = b"abcdefghijklmnopqrstuvwxyz";
     const DIGITS: &[u8] = b"0123456789";
     const SPECIAL: &[u8] = b")(*&^%$#@!~";
     const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
@@ -27,9 +41,15 @@ fn main() {
         })
         .collect();
 
-    let l = DIGITS.len() + CAPITAL.len();
-    let xs: &[u8] = &[CAPITAL, DIGITS].concat();
+    let l = CAPITAL.len() + 
+        LOWER.len() +
+        DIGITS.len() +
+        SPECIAL.len();
+    let xs: &[u8] = &[CAPITAL, LOWER, DIGITS, SPECIAL].concat();
 
+    let ch_special = SPECIAL.choose(&mut rand::thread_rng());
+    let ch_digit = rng.gen_range(0..10);
+    
     let mut result = rng.gen_range(0..10);
     let password2: String = (0..PASSWORD_LEN-1)
         .map(|_| {
@@ -56,4 +76,6 @@ fn main() {
     println!("{}", password2);
     println!("{:?}", result);
     println!("{}", s);
+
+    println!("{:?}", *ch_special.unwrap() as char);
 }
