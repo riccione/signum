d7ba5a87d347440c8a797d728ebf29488ff51764
Zero <zero@zero-ms7c56.ignes-zero.com>
2025-02-17 21:41:13 -0500

    Refactor code by introducing a new const and simplifying some constructions

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use clap::{Parser};
 use rand::{Rng};
 use rand::prelude::IndexedRandom;
 use rand::seq::SliceRandom;
+use rand::rngs::ThreadRng;
 use std::{char};
 use std::process::ExitCode;
 
@@ -20,31 +21,32 @@ struct Cli {
 }
 
 fn main() -> ExitCode {
+    const DEFAULT_LENGTH: u8 = 10;
     const CAPITAL: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const LOWER: &[u8] = b"abcdefghijklmnopqrstuvwxyz";
     const DIGITS: &[u8] = b"0123456789";
-    const SPECIAL: &[u8] = b")(*&^%$#@!~";
-    let l = CAPITAL.len() + 
-        LOWER.len() +
-        DIGITS.len() +
-        SPECIAL.len();
+    const DIGITS_LEN: usize = 10;
+    const SPECIAL: &[u8] = b")-(*&^%$#@!~";
+    let l = LOWER.len() * 2 + DIGITS_LEN + SPECIAL.len();
+
     let xs: &[u8] = &[CAPITAL, LOWER, DIGITS, SPECIAL].concat();
     let mut rng = rand::rng();
     let ch_capital = CAPITAL.choose(&mut rng);
     let ch_special = SPECIAL.choose(&mut rng);
-    let ch_digit = rng.random_range(0..10);
-
+    let ch_digit = rng.random_range(0..DIGITS_LEN);
+    
     let args = Cli::parse();
-    let password_len: usize = args.len.unwrap_or(10) as usize;
+    let password_len: usize = args.len.unwrap_or(DEFAULT_LENGTH) as usize;
    
     for _ in 0..args.num.unwrap_or(1) {
         let s: String = if args.digit {
-            get_rng(DIGITS, password_len, DIGITS.len())
+            get_rng(&mut rng, DIGITS, password_len, DIGITS.len())
         } else {
-            let mut pass: Vec<char> = get_rng(xs, password_len-3, l);
+            let mut pass: Vec<char> = get_rng(&mut rng, xs, password_len-3, l);
+            // password will include at least one digit, capital and special char
             pass.push(char::from_digit(ch_digit as u32, 10).unwrap());
-            pass.push(*ch_special.unwrap() as char);
             pass.push(*ch_capital.unwrap() as char);
+            pass.push(*ch_special.unwrap() as char);
 
             pass.shuffle(&mut rng);
             pass
@@ -54,11 +56,10 @@ fn main() -> ExitCode {
     ExitCode::SUCCESS
 }
 
-fn get_rng(xs: &[u8], l: usize, l1: usize) -> Vec<char> {
-    let mut rng = rand::rng();
+fn get_rng(r: &mut ThreadRng, xs: &[u8], l: usize, l1: usize) -> Vec<char> {
     (0..l)
         .map(|_| {
-            let idx = rng.random_range(0..l1);
+            let idx = r.random_range(0..l1);
             xs[idx] as char
         })
         .collect()
